cb75ed5c1a8dcc1815427305604ec3607575351b
Ball Mariam <mb2035480@gmail.com>
2026-06-06 20:53:45 +0000

    JS : dark mode avec localStorage, navbar dynamique au scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+console.log("JS chargé");
+console.log("clic bouton");
+
+const themeToggle = document.getElementById("themeToggle");
+const topBtn = document.getElementById("topBtn");
+if (themeToggle) {
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
+}
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (navbar && window.scrollY > 50) {
+        navbar.classList.add("navbar-scroll");
+    } else if (navbar) {
+        navbar.classList.remove("navbar-scroll");
+    }
+});
+
+if (topBtn) {
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            topBtn.style.display = "block";
+        } else {
+            topBtn.style.display = "none";
+        }
+    });
+
+    topBtn.addEventListener("click", function () {
+        console.log("clic bouton");
+        window.scrollTo(0, 0);
+    });
+
+}
